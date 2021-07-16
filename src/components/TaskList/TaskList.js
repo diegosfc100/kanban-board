4eaa51d66c8e4bfe20c988643ec9f4cb2556f436
diegosfc100 <diego.sfc100@hotmail.com>
2021-07-16 15:14:06 -0300

    Set tasks props

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,21 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './tasklist.css';
 import PropTypes from "prop-types";
 
-export default function TaskList({title}) {
-    const [count, setCount] = useState(0);
+export default function TaskList({title, onAddTask, tasks}) {
+
+    const addTask = () => {
+        onAddTask('Nova Tarefa', 'Pendente')
+    };
 
     return(
         <div className="tasklist">
             <div className="title">{title}</div>
-            <div className="content">
-                {count}
-            
-            </div>
+            <div className="content"></div>
+            {tasks.map((task) => {
+                return <div key={task.id}>{task.title}</div>
+            })}
+            <button onClick={addTask}>Adicionar Tarefa</button>
         </div>
     )
 }
 
 TaskList.propTypes = {
-    title: PropTypes.string.isRequired
+    title: PropTypes.string.isRequired,
+    onAddTask: PropTypes.func.isRequired,
+    tasks: PropTypes.array.isRequired
 };
