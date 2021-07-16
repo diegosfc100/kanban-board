0bb29396bd0517dc528868f4d4de58511f1523ce
diegosfc100 <diego.sfc100@hotmail.com>
2021-07-15 22:01:20 -0300

    Set navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
+
+import Navbar from './components/Navbar/Navbar';
+
+//Anatomia de um componente React / Básicamente uma função JS.
+//Tudo deve encapsulado numa só div, ou seja, um nó.
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <ul>
+          <li>Aprendendo React</li>
+          <li>Aprendendo sobre componentes</li>
+      </ul>
     </div>
   );
 }
