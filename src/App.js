358671499c450b6eaa039dc7ba27b5c1c8412af9
diegosfc100 <diego.sfc100@hotmail.com>
2021-07-16 14:05:59 -0300

    Set usestate hooks

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './index.css';
 
 
 import Navbar from './components/Navbar/Navbar';
 import TaskList from './components/TaskList/TaskList'
 
+const task = {
+    id: 0,
+    title: "Nova Tarefa",
+    state: "Pendente"
+}
+
 //Anatomia de um componente React / Básicamente uma função JS.
 //Tudo deve encapsulado numa só div, ou seja, um nó.
 function App() {
+    const [tasks, setTasks] = useState([]);
+
   return (
     <div className="App">
       <Navbar />
-      <div>
-        <TaskList title="A Fazer"/>
+      <div className="container">
+        <TaskList title={`A Fazer`}/>
         <TaskList title="Fazendo"/>
         <TaskList title="Feito"/>
       </div>
