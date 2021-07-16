173cddf1287f80da0dec78ea82fe14a7028986ac
diegosfc100 <diego.sfc100@hotmail.com>
2021-07-16 13:29:35 -0300

    Set usestates Hook

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './tasklist.css';
 import PropTypes from "prop-types";
 
 export default function TaskList({title}) {
+    const [count, setCount] = useState(0);
+
     return(
         <div className="tasklist">
             <div className="title">{title}</div>
-            <div className="content"></div>
+            <div className="content">
+                {count}
+            
+            </div>
         </div>
     )
 }
