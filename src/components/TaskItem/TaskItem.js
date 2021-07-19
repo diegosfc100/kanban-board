850bb7c31c38ce7c11f6adb94866bb51766e1dd1
diegosfc100 <diego.sfc100@hotmail.com>
2021-07-19 11:04:14 -0300

    Define task editing

--- src/components/TaskItem/TaskItem.js
+++ src/components/TaskItem/TaskItem.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from "prop-types"
 
 export default function TaskItem({id, title, taskState}){
+    const[isEditing, setIsEditing] = useState(false);
+    const[editableTitle, setEditableTitle] = useState(title);
 
-    return <div>{title}</div>
+    const onTitleChange = (event) => {
+        const newTitle = event.target.value;
+        setEditableTitle(newTitle);
+    }
+
+    const onKeyPress = (event) => {
+        if (event.key === "Enter"){
+            setIsEditing(false);
+        }
+    };
+
+    if (isEditing){
+        return(
+            <input
+                type="text"
+                value={editableTitle}
+                onChange={onTitleChange}
+                onKeyPress={onKeyPress}
+                />
+        );
+    } else{
+        return <div onClick={(e) => setIsEditing(true)}>{editableTitle}</div>
+    }
+        
 }
 
 TaskItem.propTypes = {
@@ -11,3 +36,4 @@ TaskItem.propTypes = {
     title: PropTypes.string.isRequired,
     taskState: PropTypes.string.isRequired
 }
+
